fc1dbf43800002e1185793ac9392d72e3ac5dea5
Markiesch <marksmeel@gmail.com>
2021-09-13 10:44:30 +0200

    📕 Added page titles

--- src/router/index.ts
+++ src/router/index.ts
@@ -11,23 +11,34 @@ const routes = [
     path: "/",
     name: "Dashboard",
     component: Dashboard,
+    meta: { title: "Notes - Dashboard" }
   },
   {
     path: "/edit/:id",
     name: "Editor",
     component: Editor,
     props: true,
+    meta: { title: "Notes - Editor" }
   },
   {
     path: "*",
     name: "not-found",
     component: NotFound,
+    meta: { title: "Notes - Not Found" }
   },
 ];
 
 const router = new VueRouter({
   mode: "history",
   routes,
+});
+
+const DEFAULT_TITLE = 'Notes - Dashboard';
+router.afterEach((to, from) => {
+  Vue.nextTick(() => {
+    // @ts-ignore
+    document.title = to.meta.title || DEFAULT_TITLE;
+  });
 });
 
 export default router;
