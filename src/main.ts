628390dae79964709ec6946343c43f91414caaec
Markiesch <marksmeel@gmail.com>
2021-09-13 10:03:48 +0200

    📑 Removed unused dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
 import Vue from 'vue';
-import './plugins/axios';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
-import "nprogress/nprogress.css"
 
 Vue.config.productionTip = false
 
